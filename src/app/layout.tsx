2733c7c2a7930197248166f1cb385d61e645cea5
Hamza Shehzad <Hamzashehzad59@gmail.com>
2025-10-02 04:06:24 +0000

    1. Copy the meta tag below, and paste it into your site's home page. It

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,7 @@ export default function RootLayout({
     <html lang="en" className="dark">
       <head>
         <meta charSet="UTF-8" />
+        <meta name="google-site-verification" content="PFQCdDqaY2pQAVgPIm7w3kVSDjcGb0qogsjari_J41U" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
